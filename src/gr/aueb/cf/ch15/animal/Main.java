b356d140e4f9a342e121bbca72443b848bb26183
DimDrk <dimdrk@gmail.com>
2024-06-04 23:45:49 +0300

    chapter 15, point, animal, person

---
+++ src/gr/aueb/cf/ch15/animal/Main.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.ch15.animal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog("Buddy", 3, "Golder Retriever");
+        dog.makeSound();
+        dog.searchForFood();
+
+        System.out.println("Name: " + dog.getName() + ", Age: " + dog.getAge() + " Breed: " + dog.getBreed());
+    }
+}
