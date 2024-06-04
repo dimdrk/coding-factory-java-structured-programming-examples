c1854ce6ba7550efcac195ed6bc43d262f8cece2
DimDrk <dimdrk@gmail.com>
2024-06-05 00:20:34 +0300

    chapter 15, shapes

---
+++ src/gr/aueb/cf/ch15/shape/Shape.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.ch15.shape;
+
+/**
+ * POJO (Plain Old Jav Object)
+ */
+public class Shape {
+
+    public void draw() {
+        System.out.println("Drawing a shape.");
+    }
+}
