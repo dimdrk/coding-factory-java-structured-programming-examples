2d73ffbb821b81479b53dca2fbc2afe7a99d9326
DimDrk <dimdrk@gmail.com>
2024-06-16 19:24:06 +0300

    functional interfaces, default methods

---
+++ src/gr/aueb/cf/ch16/defaultmethods/IWelcome.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.ch16.defaultmethods;
+
+public interface IWelcome {
+    void saySomething(String message);
+
+    default void sayHelloCoding() {
+        System.out.println("Hello");
+        sayCoding();
+    }
+
+    private void sayCoding() {
+        System.out.println("Coding");
+    }
+    static void sayHelloCF() {
+        System.out.println("Hello");
+        sayCF();
+    }
+
+    private static void sayCF(){
+        System.out.println("CF");
+    }
+}
