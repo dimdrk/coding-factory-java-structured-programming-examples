9a4dafeb66aaaab399752c0b4d2fd6aeff042cbf
DimDrk <dimdrk@gmail.com>
2024-07-08 23:25:38 +0300

    Lambdas, teachers

---
+++ src/gr/aueb/cf/ch20/lambdas/Main.java
@@ -0,0 +1,79 @@
+package gr.aueb.cf.ch20.lambdas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Main {
+
+    private static final List<Teacher> teachers = List.of(new Teacher(1, "A.", "Cooper"),
+            new Teacher(2, "B.", "Dylan"),
+            new Teacher(3, "C.", "Mask"),
+            new Teacher(4, "D.", "Gates"));
+
+    public static void main(String[] args) {
+//        filterAndPrintEqualsId(teachers, 4);
+//        filterAndPrintGTId(teachers, 2);
+
+//        getFilteredTeachers(teachers, new ITeacherFilter() {
+//            @Override
+//            public boolean filterId(Teacher teacher) {
+//                return teacher.getTeacherId() == 3;
+//            }
+//        });
+
+        List<Teacher> teachersList = getFilteredTeachers(teachers, teacher -> teacher.getTeacherId() <= 3);
+
+        for (Teacher teacher : teachersList) {
+            teacher.printInstanceTeacher();
+        }
+    }
+
+//    public static List<Teacher> getFilteredTeachers(List<Teacher> teachers, ITeacherFilter filter) {
+//        List<Teacher> teachersToReturn = new ArrayList<>();
+//
+//        for (Teacher teacher : teachers) {
+//            if (filter.filterId(teacher)) {
+//                teachersToReturn.add(teacher);
+//            }
+//        }
+//
+//        return teachersToReturn;
+//    }
+
+    public static List<Teacher> getFilteredTeachers(List<Teacher> teachers, Predicate<Teacher> filter) {
+        List<Teacher> teachersToReturn = new ArrayList<>();
+
+        for (Teacher teacher : teachers) {
+            if (filter.test(teacher)) {
+                teachersToReturn.add(teacher);
+            }
+        }
+
+        return teachersToReturn;
+    }
+
+    public static void filterAndPrintEqualsId(List<Teacher> teachers, int id) {
+        for (Teacher teacher : teachers) {
+            if (teacher.getTeacherId() == id) {
+                System.out.println("Full name: " + teacher.getFirstname() + ", " + teacher.getLastname());
+            }
+        }
+    }
+
+    public static void filterAndPrintLTId(List<Teacher> teachers, int id) {
+        for (Teacher teacher : teachers) {
+            if (teacher.getTeacherId() <= id) {
+                System.out.println("Full name: " + teacher.getFirstname() + ", " + teacher.getLastname());
+            }
+        }
+    }
+
+    public static void filterAndPrintGTId(List<Teacher> teachers, int id) {
+        for (Teacher teacher : teachers) {
+            if (teacher.getTeacherId() >= id) {
+                System.out.println("Full name: " + teacher.getFirstname() + ", " + teacher.getLastname());
+            }
+        }
+    }
+}
