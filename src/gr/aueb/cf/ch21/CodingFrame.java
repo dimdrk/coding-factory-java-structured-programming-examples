f7d61e863b55c70817b746c8c4bde03b4a7d7172
DimDrk <dimdrk@gmail.com>
2024-07-11 15:40:32 +0300

    CodingFrame message btn, pop up

--- src/gr/aueb/cf/ch21/CodingFrame.java
+++ src/gr/aueb/cf/ch21/CodingFrame.java
@@ -1,6 +1,8 @@
 package gr.aueb.cf.ch21;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CodingFrame extends JFrame {
 
@@ -9,5 +11,18 @@ public class CodingFrame extends JFrame {
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Hello Coding Factory");
+
+        JPanel contentPane = new JPanel();
+        this.setContentPane(contentPane);
+
+        JButton messageBtn = new JButton("Message Button");
+        messageBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "This is a message", "Title of Message", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
+        contentPane.add(messageBtn);
     }
 }
