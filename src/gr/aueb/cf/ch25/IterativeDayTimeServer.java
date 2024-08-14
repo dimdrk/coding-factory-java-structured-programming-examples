237d021ab4a28d3dafe5640b566cc640ba8ee1aa
DimDrk <dimdrk@gmail.com>
2024-08-14 17:25:54 +0300

    DateTime Server added message

--- src/gr/aueb/cf/ch25/IterativeDayTimeServer.java
+++ src/gr/aueb/cf/ch25/IterativeDayTimeServer.java
@@ -5,5 +5,6 @@ public class IterativeDayTimeServer {
     public static void main(String[] args) {
         DateTimeServer dateTimeServer = new DateTimeServer();
         dateTimeServer.start();
+        System.out.println("DateTime Server has started...");
     }
 }
