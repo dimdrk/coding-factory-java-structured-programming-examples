b18dfc04b4596fbe48b0c09f0d97413bbdb5d38e
DimDrk <dimdrk@gmail.com>
2024-08-14 17:10:33 +0300

    Date time client turned to localhost, Iterative thread added

--- src/gr/aueb/cf/ch25/DateTimeCli.java
+++ src/gr/aueb/cf/ch25/DateTimeCli.java
@@ -14,7 +14,8 @@ public class DateTimeCli {
         StringBuilder sb = new StringBuilder();
 
         try {
-            InetAddress servAddress = InetAddress.getByName("time.nist.gov");
+            // InetAddress servAddress = InetAddress.getByName("time.nist.gov");
+            InetAddress servAddress = InetAddress.getByName("127.0.0.1");
             int servPort = 13;
 
             socket = new Socket(servAddress, servPort);
@@ -25,7 +26,7 @@ public class DateTimeCli {
             while ((line = bf.readLine()) != null) {
                 sb.append(line).append("\n");
             }
-            System.out.println(sb);
+            System.out.println("Local DateTime Server says: " + sb);
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
